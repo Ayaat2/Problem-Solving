6de036df785fc7b5fc83effda4f8a38322bbddf9
Ayaat2 <halawany.986@gmail.com>
2026-05-23 16:11:39 +0300

    Add simple array sum solution

---
+++ hackerRank/simpleArraySum.js
@@ -0,0 +1,76 @@
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array , , so return .
+
+// **Function Description**
+
+// Complete the  function with the following parameter(s):
+
+// - : an array of integers
+
+// **Returns**
+
+// - : the sum of the array elements
+
+// **Input Format**
+
+// The first line contains an integer, , denoting the size of the array.
+// The second line contains  space-separated integers representing the array's elements.
+
+// **Constraints**
+
+// **Sample Input**
+
+// ```
+// STDIN           Function
+// -----           --------
+// 6               ar[] size n = 6
+// 1 2 3 4 10 11   ar = [1, 2, 3, 4, 10, 11]
+// ```
+
+// **Sample Output**
+
+// ```
+// 31
+// ```
+
+// **Explanation**
+
+// Print the sum of the array's elements: 1 + 2 + 3+ 4 + 10 + 11= 31.
+
+// ```
+// function simpleArraySum(ar) {
+
+//     let sum = 0;
+
+//     // ar.map((num)=>{
+//     //     sum+=num;
+//     // })
+//     // return sum;
+//
+//     // or
+
+//     for(let i=0; i < ar.length; i++){
+//         sum+=ar[i];
+//     }
+//     return sum;
+// }
+
+// console.log(simpleArraySum([1,2,3,4,10,11]));
+// ```
+
+function simpleArraySum(ar) {
+  let sum = 0;
+
+  // ar.map((num)=>{
+  //     sum+=num;
+  // })
+  // return sum;
+
+  // or
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+  return sum;
+}
+console.log(simpleArraySum([1, 2, 3, 4, 10, 11]));

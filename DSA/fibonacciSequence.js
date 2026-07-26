ed10237309f1d2bf6b05b0a3129e46cd234253a5
Ayaat2 <halawany.986@gmail.com>
2026-07-26 12:41:19 +0300

    update in the solution and the algorithm

--- DSA/fibonacciSequence.js
+++ DSA/fibonacciSequence.js
@@ -1,44 +1,47 @@
-// ## 📝 Problem Statement
+//📝 Problem
 
-// > **Given a number `n`, return the first `n` numbers of the Fibonacci sequence.**
-// >
-// > The Fibonacci sequence starts with **0** and **1**, and each subsequent number is the sum of the previous two numbers.
+//  **Given a number `n`, return the first `n` numbers of the Fibonacci sequence.**
+//  The Fibonacci sequence starts with **0** and **1**, and each subsequent number is the sum of the previous two numbers.
 
-// ### Examples
+// Examples
 
-// ```
 // fibonacci(2)
-
 // Output:
 // [0, 1]
-// ```
 
-// ```
 // fibonacci(3)
-
 // Output:
 // [0, 1, 1]
-// ```
 
-// ```
 // fibonacci(7)
-
 // Output:
 // [0, 1, 1, 2, 3, 5, 8]
 
 // the algorithm :
 
-// Step 1: Initialize the array with the first two elements
-// Step 2: Loop from index 2 up to n
-// Step 3 & 4: Sum the last two elements and push to the array
-// Step 5: Return the resulting array
+// 1. If n = 0 return []
+// 2. If n = 1 return [0]
+// 3. Initialize the array with the first start [0,1]
+// 4. loop from index 2 up to n
+// 5. Each time, calculate the sum of the last two numbers
+// 6. Push the result
+// 7. return the array.
 
-const fibonacci = (n) => {
-  let fib = [0, 1];
+const fibonacci=(n)=>{
+  
+  if(n===0) return [];
+  if(n===1) return [0];
 
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+  let fib=[0,1];
+  
+  for(let i=2; i < n; i++){
+    fib[i]=fib[i-1] + fib[i-2];
   }
   return fib;
-};
-console.log(fibonacci(7)); // [ 0, 1, 1, 2, 3, 5, 8 ]
+}
+
+console.log(fibonacci(7)); // [0, 1, 1, 2, 3, 5, 8]
+console.log(fibonacci(0)); // []
+console.log(fibonacci(1)); // [0]
+console.log(fibonacci(5)); // [0, 1, 1, 2, 3]
+
